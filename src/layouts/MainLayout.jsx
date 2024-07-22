1909d994aabfc14ff6f5e830c8b11ba68389f852
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-22 14:56:50 +0300

    updtae

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 const MainLayout = () => {
   return (
-    <div>
-      main layout
-    </div>
+    <>
+    <Navbar/>
+     <Outlet/>
+    </>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import HomeCards from '../components/HomeCards'
+
 const HomePage = () => {
   return (
     <>
      <Hero/>
+     <HomeCards/>
     </>
   )
 }
