f094fef63f811dd4f0b5d11e0fdb23465139f675
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-22 15:01:12 +0300

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import HomeCards from '../components/HomeCards'
-
+import JobListings from '../components/JobListings'
 const HomePage = () => {
   return (
     <>
      <Hero/>
      <HomeCards/>
+     <JobListings/>
     </>
   )
 }
