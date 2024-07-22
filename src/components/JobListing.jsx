3a8242cddeba4d474ce642409a922442b8458937
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-22 09:48:49 +0300

    update

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const JobListing = ({job}) => {
+
+  const [showFullDescription,setShowFullDescription] =useState(false);
+
+  let description = job.description;
+  if (!showFullDescription){
+    description= description.substring(0,90)+'...';
+  }
   return (
     <div className="bg-white rounded-xl shadow-md relative ">
     <div className="p-4">
@@ -10,7 +19,7 @@ const JobListing = ({job}) => {
       </div>
 
       <div className="mb-5">
-      {job.description}
+      {description}
       </div>
 
       <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
