f360b9630deab63eac99cfd2a267ac1024f2b38b
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-24 10:12:28 +0300

    update

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import {FaMapMarker} from 'react-icons/fa'
-
+import { Link } from 'react-router-dom'
 const JobListing = ({job}) => {
 
   const [showFullDescription,setShowFullDescription] =useState(false);
@@ -33,12 +33,12 @@ className='text-indigo-500 mb-5 hover:text-indigo-600'>
          <FaMapMarker className='inline text-lg mb-1 mr-1'/>
          {job.location}
         </div>
-        <a
-          href={`/jobs/${job.id}`}
+        <Link
+          to={`/jobs/${job.id}`}
           className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
          Read More
-        </a>
+        </Link>
       </div>
     </div>
   </div>
