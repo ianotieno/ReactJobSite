c8b47bc0e377b6a2e58b4010f0a92b2d5c76fe6e
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-24 10:19:18 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../assets/images/logo.png'
 import { NavLink } from 'react-router-dom';
 const Navbar = () => {
+  const LinkClass= ({isActive })=> isActive? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2':'text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -24,18 +25,18 @@ const Navbar = () => {
               <div className="flex space-x-2">
                 <NavLink
                   to="/"
-                  className={({isActive })=> isActive? 'text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2':'text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'}
+                  className={LinkClass}
                 
                   >Home</NavLink
                 >
                 <NavLink
                   to="/jobs"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={LinkClass}
                   >Jobs</NavLink
                 >
                 <NavLink
                   to="/add-job"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={LinkClass}
                   >Add Job</NavLink
                 >
               </div>
