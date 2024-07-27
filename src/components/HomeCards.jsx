9855729f7bca2bac55dc61f00254b572e63b5bb3
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-27 13:14:29 +0300

    update

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -5,7 +5,7 @@ const HomeCards = () => {
   return (
     <section className="py-2">
       <div className="container-xl lg:container hw  m-auto md:grid-cols-3">
-        <div className="flex  justify-between p-1  rounded-lg ">
+        <div className="flex justify-between p-1 rounded-lg ">
           <Cards>
             <h2 className="text-2xl font-bold">For Developers</h2>
             <p className="mt-2 mb-4">
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -12,7 +12,7 @@ const JobListing = ({job}) => {
   }
   return (
     <div className="bg-white rounded-xl shadow-md relative ">
-    <div className="p-4">
+    <div className=" p-4">
       <div className="mb-6">
         <div className="text-gray-600 my-2">{job.type}</div>
         <h3 className="text-xl font-bold">{job.title}</h3>
