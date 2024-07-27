d2ce49bf4231594bf80b90a2aeca6933a6e4ce94
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-27 21:19:15 +0300

    update

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 //import { useState,useEffect } from 'react';
 import { FaArrowLeft,FaMapMarker } from 'react-icons/fa';
-import {useParams, useLoaderData} from 'react-router-dom'
+import {useParams, useLoaderData, useNavigate} from 'react-router-dom'
 import { Link } from 'react-router-dom';
 
-const JobPage = () => {
+const JobPage = ({deleteJob}) => {
   const {id} =useParams();
   const job = useLoaderData();
+  const navigate= useNavigate();
+  const onDeleteClick= (jobId)=>{
+    const confirm= window.confirm("Are you sure you want to delete this listing ?")
+    if (!confirm) return;
+    deleteJob(jobId);
+    navigate('/jobs')
+  }
   //const [job,setJob]= useState(null);
   //const [loading, setLoading] = useState(true);
 
@@ -103,7 +110,7 @@ fetchJob();
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link
               >
-              <button
+              <button onClick={()=>onDeleteClick(job.id)}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
