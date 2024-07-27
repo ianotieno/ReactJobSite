fdc28f42bd77bc2b3359e17c8aa3a77d8e06fd62
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-27 11:33:55 +0300

    update server

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -6,7 +6,7 @@ const JobListings = ({isHome= false}) => {
 const jobListings= isHome ? jobs.slice(0,3):jobs;
 
   return (
-    <section className="bg-blue-50 px-4 py-10 sm:flex-col lg:flex-row">
+    <section className="bg-blue-50 px-4 py-10 sm:flex-col lg:flex-col">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           Browse Jobs
