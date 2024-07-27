e282ad06f17519ff241ac6dc093b9652390c4f4c
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-27 18:06:04 +0300

    update

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { useState,useEffect } from 'react';
-import {useParams} from 'react-router-dom'
+//import { useState,useEffect } from 'react';
+import {useParams, useLoaderData} from 'react-router-dom'
 import Spinner from '../components/Spinner';
 const JobPage = () => {
   const {id} =useParams();
-  const [job,setJob]= useState(null);
-  const [loading, setLoading] = useState(true);
+  const job = useLoaderData();
+  //const [job,setJob]= useState(null);
+  //const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
+ /* useEffect(()=>{
     const fetchJob =async ()=>{
       try {
         const res = await fetch(`/api/jobs/${id}`)
@@ -20,11 +21,17 @@ const JobPage = () => {
       }
     }
 fetchJob();
-  }, [])
+  }, [])*/
   return (
-    loading? <Spinner/> :(<h1>{job.title}</h1>)
+   (<h1>{job.title}</h1>)
   )
 }
+const  jobLoader= async ({params})=>
+  {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data= await res.json();
+    return data;
 
+  }
 
-export default JobPage
+export{JobPage as default , jobLoader} ;
