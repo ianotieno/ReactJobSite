47ef283270ec7c906eec0a44009ccbf6ce43ba27
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-27 17:44:25 +0300

    update

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState,useEffect } from 'react';
 import {useParams} from 'react-router-dom'
+import Spinner from '../components/Spinner';
 const JobPage = () => {
   const {id} =useParams();
   const [job,setJob]= useState(null);
@@ -9,7 +10,7 @@ const JobPage = () => {
   useEffect(()=>{
     const fetchJob =async ()=>{
       try {
-        const res = await fetch(`/api/job/${id}`)
+        const res = await fetch(`/api/jobs/${id}`)
         const data= await res.json();
         setJob(data);
       } catch (error) {
@@ -21,9 +22,7 @@ const JobPage = () => {
 fetchJob();
   }, [])
   return (
-    <div>
-      test
-    </div>
+    loading? <Spinner/> :(<h1>{job.title}</h1>)
   )
 }
 
